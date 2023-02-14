bbe1a2dc3d228562c43d7e28f48393e2338c0cb0
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 11:46:41 +0530

    switch case

---
+++ index.js
@@ -0,0 +1,21 @@
+var level=1;
+
+switch(level){
+    case 1:
+    console.log("level1");
+    break;
+    case 2:
+    console.log("level2");
+    break;
+    case 3:
+    console.log("level3");
+    break;
+    case 4:
+    console.log("level4");
+    break;
+    case 5:
+    console.log("level5");
+    break;
+    default:
+    console.log("start playing");
+}
